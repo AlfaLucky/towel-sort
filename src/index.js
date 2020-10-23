c7209869bf0698eb6220ce7c728661726fbbfbb2
AlfaLucky <shiryaaa@mail.ru>
2020-10-23 18:10:48 +0300

    feat: added Towel Sort
    https://github.com/AlfaLucky/towel-sort

--- src/index.js
+++ src/index.js
@@ -2,5 +2,14 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+    if (!(Array.isArray(matrix)) || matrix === []) return [];
+    let sortedMatrix = [];
+    for (i = 0; i < matrix.length; i++) {
+      if ( i % 2 === 0) {
+        sortedMatrix = sortedMatrix.concat(matrix[i]);
+      } else {
+        sortedMatrix = sortedMatrix.concat(matrix[i].reverse());
+      }
+    }
+    return sortedMatrix;
 }
